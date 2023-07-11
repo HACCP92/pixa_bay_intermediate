f81737b67170c3333d242585de8acac03c57072a
HACCP92 <111282517+HACCP92@users.noreply.github.com>
2023-07-12 00:35:31 +0900

    [이미지 검색 앱] main view model 수정

--- lib/core/router/router.dart
+++ lib/core/router/router.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:go_router/go_router.dart';
+import 'package:pixa_bay_intermediate/data/repository/pixabay_photo_repository_impl.dart';
+import 'package:pixa_bay_intermediate/domain/use_case/get_top_five_most_viewd_images_use_case.dart';
 import 'package:pixa_bay_intermediate/presentation/detail/detail_screen.dart';
 import 'package:pixa_bay_intermediate/presentation/main/main_screen.dart';
 import 'package:pixa_bay_intermediate/presentation/main/main_view_model.dart';
@@ -11,7 +13,11 @@ final GoRouter router = GoRouter(
       path: '/',
       builder: (BuildContext context, GoRouterState state) {
         return ChangeNotifierProvider(
-          create: (_) => MainViewModel(),
+          create: (_) => MainViewModel(
+            GetTopFiveMostViewdImagesUseCase(
+              PixabayPhotoRepositoryImpl(),
+            ),
+          ),
           child: const MainScreen(),
         );
       },
