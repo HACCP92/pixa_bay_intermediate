889c95f71eb6aea12677f69f3dfda377655db39d
HACCP92 <111282517+HACCP92@users.noreply.github.com>
2023-07-12 00:32:57 +0900

    [이미지 검색 앱] copywith 메소드를 활용해서 main view model 구현

--- lib/presentation/main/main_view_model.dart
+++ lib/presentation/main/main_view_model.dart
@@ -1,3 +1,22 @@
 import 'package:flutter/material.dart';
+import 'package:pixa_bay_intermediate/domain/use_case/get_top_five_most_viewd_images_use_case.dart';
+import 'package:pixa_bay_intermediate/presentation/main/main_state.dart';
 
-class MainViewModel with ChangeNotifier {}
+class MainViewModel with ChangeNotifier {
+  final GetTopFiveMostViewdImagesUseCase _getTopFiveMostViewdImagesUseCase;
+
+  MainViewModel(this._getTopFiveMostViewdImagesUseCase);
+  MainState _state = const MainState();
+
+  MainState get state => _state;
+
+  void fetchImages(String query) async {
+    _state = state.copyWith(isLoading: true);
+    notifyListeners();
+
+    _state = state.copyWith(
+        photos: await _getTopFiveMostViewdImagesUseCase.execute(query),
+        isLoading: false);
+    notifyListeners();
+  }
+}
